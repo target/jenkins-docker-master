1096aadda942751af2c0ce6e390c09f11dc45e09
JordanSussman <jordan.sussman@target.com>
2017-10-27 14:05:16 -0500

    Another update since Fridays are hard

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # This is a simple script to update the version of jenkins being used
 
 JENKINS_VER=2.73.2
-JENKINS_REL=1
+JENKINS_REL=2
 
 sed -i '' -E "s/\`(debug-)?latest\`, \`(debug-)?[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,2}-[0-9]{1,2}\`/\`\1latest\`, \`\1${JENKINS_VER}-${JENKINS_REL}\`/g" README.md
 sed -i '' -E "s/JENKINS_VER=(.*)/JENKINS_VER=${JENKINS_VER}/g; s/JENKINS_REL=(.*)/JENKINS_REL=${JENKINS_REL}/g" Dockerfile
