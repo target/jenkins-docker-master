34a94671cd0973afa027321898e66e6444c2beb2
David May <david.may@target.com>
2020-02-24 11:36:48 -0600

    fix(update): fix update script

--- update.sh
+++ update.sh
@@ -4,11 +4,13 @@
 JENKINS_VER=2.190.1
 RELEASE=1
 
+### DO NOT MODIFY ANY LINES BELOW ###
+
 for fname in Dockerfile Dockerfile.debug files/jenkins_wrapper.sh examples/Dockerfile examples/files/jenkins_wrapper.sh
 do
-    sed -i'' -E "s/JENKINS_VER=(.*)/JENKINS_VER=${JENKINS_VER}/g; s/RELEASE=(.*)/RELEASE=${RELEASE}/g" $fname
+    sed -i '' -E "s/JENKINS_VER=(.*)/JENKINS_VER=${JENKINS_VER}/g; s/RELEASE=(.*)/RELEASE=${RELEASE}/g" $fname
 done
 
-sed -i'' -E "s/(jenkins-docker-master:)[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,2}-[0-9]{1,2}/jenkins-docker-master:${JENKINS_VER}-${RELEASE}/g" README.md
-sed -i'' -E "s/debug-[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,2}-[0-9]{1,2}/debug-${JENKINS_VER}-${RELEASE}/g" README.md
-sed -i'' -E "s/jenkins\/jenkins:[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,2}/jenkins\/jenkins:${JENKINS_VER}/g" README.md
+sed -i '' -E "s/(jenkins-docker-master:)[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,2}-[0-9]{1,2}/jenkins-docker-master:${JENKINS_VER}-${RELEASE}/g" README.md
+sed -i '' -E "s/debug-[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,2}-[0-9]{1,2}/debug-${JENKINS_VER}-${RELEASE}/g" README.md
+sed -i '' -E "s/jenkins\/jenkins:[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,2}/jenkins\/jenkins:${JENKINS_VER}/g" README.md
